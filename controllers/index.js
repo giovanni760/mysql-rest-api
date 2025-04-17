bb67309ffac09f15384b7da0bbffe0bcac3e10b7
giovanni760 <giovanni162005@gmail.com>
2025-04-17 05:04:39 -0600

    update and add delete, update, getbyid

--- controllers/index.js
+++ controllers/index.js
@@ -1,34 +1,86 @@
 const models = require("../database/models");
 
 const createUser = async (req, res) => {
-console.log('creating user');
+   console.log('creating user');
 
-try {
-    const user = await models.User.create(req.body);
-    return res.status(201).json({ user });
-
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
+   try {
+      const user = await models.User.create(req.body);
+      return res.status(201).json({ user });
+   } catch (error) {
+      return res.status(500).send({ error: error.message });
+   }
 };
 
+const deleteUser = async (req, res) => {
+   console.log('deleting user...');
+
+   try {
+      const user = await models.User.findOne({ where: { id: req.params.id } });
+      if (user) {
+         console.log(user);
+         await user.destroy();
+         return res.status(200).json({ "deleted ": req.params.id });
+      } else {
+         return res.status(200).json({ "error ": req.params.id + " no existe" });
+      }
+   } catch (error) {
+      return res.status(500).send({ error: error.message });
+   }
+};
+
+const updateUser = async (req, res) => {
+   console.log('updating user...');
+
+   try {
+      const user = await models.User.findOne({ where: { id: req.params.id } });
+      if (user) {
+         console.log(user);
+         user.name = req.body.name;
+         user.email = req.body.email;
+         user.age = req.body.age;
+         user.comments = req.body.comments;
+         await user.save();
+         return res.status(200).json({ "updated ": user });
+      } else {
+         return res.status(200).json({ "error ": req.params.id + " no existe" });
+      }
+   } catch (error) {
+      return res.status(500).send({ error: error.message });
+   }
+};
 
 const getAllUsers = async (req, res) => {
-  console.log('getting users');
-  try {
-    const users = await models.User.findAll({
-      include:  []
-    });
-   
-     return res.status(200).json({ users });
-  }
-   catch (error) {
-    return res.status(500).send(error.message);
-  }
+   console.log('getting users');
+
+   try {
+      const users = await models.User.findAll({
+         include: []
+      });
+      return res.status(200).json({ users });
+   } catch (error) {
+      return res.status(500).send(error.message);
+   }
 };
 
+const getUser = async (req, res) => {
+   console.log('getting user...');
+
+   try {
+      const user = await models.User.findOne({ where: { id: req.params.id } });
+      if (!user) {
+         return res.status(200).json({ "error": req.params.id + " no existe" });
+      }
+      return res.status(200).json({ user });
+   } catch (error) {
+      return res.status(500).send({ error: error.message });
+   }
+};
 
 module.exports = {
-  createUser, 
-  getAllUsers
+   createUser,
+   getAllUsers,
+   deleteUser,
+   updateUser,
+   getUser
 };
+
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ router.get('/', (req, res) => res.send('Welcome'));
 
 router.post('/users', controllers.createUser);
 router.get('/users', controllers.getAllUsers);
+router.get('/users/:id', controllers.getUser);
+router.delete('/users/:id', controllers.deleteUser);
+router.put('/users/:id', controllers.updateUser);
 
 module.exports = router;
 
